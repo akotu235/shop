5302d78115fcc5e73cc2b0c62428e6d71fe1ecf7
akotu235 <akotu@protonmail.com>
2024-09-17 17:11:11 +0200

    Created Home Controller

---
+++ src/main/java/io/github/akotu235/shop/controller/HomeController.java
@@ -0,0 +1,18 @@
+package io.github.akotu235.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    @GetMapping("/about")
+    public ModelAndView about() {
+        return new ModelAndView("about");
+    }
+
+    @GetMapping("/contact")
+    public ModelAndView contact() {
+        return new ModelAndView("contact");
+    }
+}
