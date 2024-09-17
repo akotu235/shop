b68acfd3a71bd6fc3a9f29fa575d498a548f064f
akotu235 <akotu@protonmail.com>
2024-09-17 16:37:45 +0200

    Created Category Entity Model

---
+++ src/main/java/io/github/akotu235/shop/service/shop/entity/Category.java
@@ -0,0 +1,55 @@
+package io.github.akotu235.shop.service.shop.entity;
+
+import io.github.akotu235.shop.service.shop.projection.write.NewCategoryWriteModel;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products;
+
+    public Category(NewCategoryWriteModel newCategory) {
+        this.name = newCategory.getName();
+        this.products = new ArrayList<>();
+    }
+
+    public Category() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
