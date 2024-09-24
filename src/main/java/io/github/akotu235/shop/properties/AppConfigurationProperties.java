e4163928e8b3fa0a54d7954fcf1d4495a8f739b2
akotu235 <akotu@protonmail.com>
2024-09-24 12:12:18 +0200

    Added configuration property setting supported languages

--- src/main/java/io/github/akotu235/shop/properties/AppConfigurationProperties.java
+++ src/main/java/io/github/akotu235/shop/properties/AppConfigurationProperties.java
@@ -3,12 +3,15 @@ package io.github.akotu235.shop.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 @ConfigurationProperties("app")
 public class AppConfigurationProperties {
     private String title;
     private String url;
     private String dataPath;
+    private List<String> supportedLanguages;
 
     public String getTitle() {
         return title;
@@ -32,5 +35,13 @@ public class AppConfigurationProperties {
 
     public void setDataPath(String dataPath) {
         this.dataPath = dataPath;
+    }
+
+    public List<String> getSupportedLanguages() {
+        return supportedLanguages;
+    }
+
+    public void setSupportedLanguages(List<String> supportedLanguages) {
+        this.supportedLanguages = supportedLanguages;
     }
 }
