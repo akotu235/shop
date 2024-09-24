3c1cbee391190c79e25e14dd01987b04d75ceea4
akotu235 <akotu@protonmail.com>
2024-09-24 12:18:23 +0200

    Added global supported languages attribute to the model

--- src/main/java/io/github/akotu235/shop/controller/advice/GlobalControllerAdvice.java
+++ src/main/java/io/github/akotu235/shop/controller/advice/GlobalControllerAdvice.java
@@ -6,17 +6,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.List;
+import java.util.Locale;
+
 @ControllerAdvice
 public class GlobalControllerAdvice {
     private final ShopService shopService;
+    private final List<Locale> supportedLocales;
 
-    public GlobalControllerAdvice(ShopService shopService) {
+    public GlobalControllerAdvice(ShopService shopService, List<Locale> supportedLocales) {
         this.shopService = shopService;
+        this.supportedLocales = supportedLocales;
     }
 
     @ModelAttribute
     public void addAttributes(Model model, Authentication authentication) {
         model.addAttribute("categories", shopService.getCategories());
+        model.addAttribute("supportedLanguages", supportedLocales);
         if (authentication != null) {
             model.addAttribute("cart", shopService.getCart(authentication));
         }
