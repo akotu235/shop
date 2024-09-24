708b0c91e0db96967b863fc24dbb3d24375f073e
akotu235 <akotu@protonmail.com>
2024-09-24 12:20:52 +0200

    Created language change button

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -5,3 +5,18 @@ function toggleMenu() {
     menu.classList.toggle("active");
     button.classList.toggle("open");
 }
+
+
+const language = document.getElementById('language');
+const languageContainer = document.getElementById('language-container');
+const body = document.getElementsByTagName('body')[0];
+
+language.addEventListener('mouseenter', () => {
+    language.style.display = 'none';
+    languageContainer.style.display = 'flex';
+});
+
+languageContainer.addEventListener('mouseleave', () => {
+    language.style.display = 'flex';
+    languageContainer.style.display = 'none';
+});
