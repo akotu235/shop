9a02d9f5d08f82e4850d6434a0eb544887358514
akotu <akotu@protonmail.com>
2025-03-06 16:58:25 +0100

    Replaced deprecated Spring Security methods with lambda-based configuration

--- src/main/java/io/github/akotu235/shop/configuration/SecurityConfig.java
+++ src/main/java/io/github/akotu235/shop/configuration/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,24 +18,25 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.securityMatcher("/**").csrf().disable();
-        http.headers().frameOptions().sameOrigin();
-        http.authorizeHttpRequests()
-                .requestMatchers("/seller-panel/**").hasRole("SELLER")
-                .and().authorizeHttpRequests()
-                .requestMatchers("/user/**").authenticated()
-                .and().authorizeHttpRequests()
-                .requestMatchers("/cart/**").authenticated()
-                .and().authorizeHttpRequests()
-                .requestMatchers("/order/**").authenticated()
-                .anyRequest().permitAll();
-        http.formLogin()
-//                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/")
-                .permitAll();
+        http
+                .securityMatcher("/**")
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/seller-panel/**").hasRole("SELLER")
+                        .requestMatchers("/user/**").authenticated()
+                        .requestMatchers("/cart/**").authenticated()
+                        .requestMatchers("/order/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .permitAll()
+                );
         return http.build();
     }
 
